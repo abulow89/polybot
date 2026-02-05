8eeafefa7649863cd82363890fb335d44720a0a9
abulow89 <39782554+abulow89@users.noreply.github.com>
2026-02-06 01:16:01 +1000

    Update postOrder.ts

--- src/utils/postOrder.ts
+++ src/utils/postOrder.ts
@@ -127,7 +127,7 @@ const orderArgs = {
   const signedOrder = await createOrderWithRetry(clobClient, orderArgs);
   if (!signedOrder) return 0;
 
-  const resp = await safeCall(() => clobClient.postOrder(signedOrder, OrderType.FAK));
+  const resp = await safeCall(() => clobClient.postOrder(signedOrder, OrderType.FOK));
   if (!resp.success) {
     console.log('Error posting order:', resp.error ?? resp);
     return 0;
@@ -190,7 +190,7 @@ const executeSmartOrder = async (
     bestPrice,
     feeRateBps,
     marketMinSize,
-    OrderType.FAK,
+    OrderType.FOK,
     availableBalance,
     feeMultiplier
   );
