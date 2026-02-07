c593d8e3cf20c4a0ca86663e7cbb7245169b8e98
abulow89 <39782554+abulow89@users.noreply.github.com>
2026-02-08 09:30:20 +1000

    Update postOrder.ts

--- src/utils/postOrder.ts
+++ src/utils/postOrder.ts
@@ -258,7 +258,7 @@ try {
   // Use getBalanceAllowance instead of getBalance
   const bal = await safeCall(() =>
     clobClient.getBalanceAllowance({
-      asset_type: AssetType.CONDITIONAL,
+      asset_type: AssetType."CONDITIONAL",
       token_id: tokenId,
     })
   );
